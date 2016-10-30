63f87ba93e114639705a8decbb7e69bf3b058390
popaliviu <yo_liviu_96@yahoo.com>
2016-10-30 22:24:14 +0200

    Create T1_ex3_Liviu_Popescu.m

---
+++ T1_ex3_Liviu_Popescu.m
@@ -0,0 +1,25 @@
+N = 4;
+    ts = 0.001;
+    t = 0:ts:N;
+    
+    k = 1;
+    
+    for n = 1:length(t)
+     if  t(n) <= k*0.25
+         if k <= length(a)
+             x1(n) = a(k);
+         else
+             x1(n) = 0;
+         end
+     else
+        if k <= length(a)
+             x1(n) = a(k);
+        end
+        k = k+1; 
+     end
+    end
+    
+    axis([0 20 -5 3])
+    plot(t,x1);
+   
+end
